e0717997f3c88290f6e9d7ff72a8784503024ac7
julianxlozano <julianxlozano@gmail.com>
2021-04-05 13:33:42 -0500

    commit tree d5689ff4c87a1e27da45fe4c09b06ca309d6b7a1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 
 require_relative '../../config/environment'
+require 'pry'
 
 class ApplicationController < Sinatra::Base
 
@@ -10,4 +11,41 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles' do
+    @articles = Article.all 
+    erb :index
+  end
+  get '/articles/new' do 
+    erb :new 
+  end
+
+  get '/articles/:id' do 
+    @article = Article.find(params[:id]) 
+    erb :show 
+  end 
+
+  post '/articles' do
+    @article = Article.create(title:params[:title],content:params[:content])
+    redirect :"articles/#{@article.id}"
+  end
+
+  get '/articles/:id/edit' do 
+    @article = Article.find(params[:id])
+    erb :edit 
+  end
+
+  patch '/articles/:id' do 
+   # binding.pry
+  @article = Article.update(params[:id],title:params[:title],content:params[:content])
+  @article.save 
+    redirect :"articles/#{@article.id}"
+  end 
+
+  delete '/articles/:id' do 
+   # binding.pry
+    Article.delete(params[:id])
+  end 
+
+
 end
